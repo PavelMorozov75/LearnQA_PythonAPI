d6d8e5983d24a63b35f457ff951cca5e5e06fddd
pavelmorozov <pavelmorozov@rambler.ru>
2023-10-22 12:01:01 +0300

    Final version of example

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,14 +1,13 @@
 import json.decoder
-import requests
 from requests import Response
 class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"cant find cookie with name {cookie_name} in the last response"
-        return response.cookies['cookie_name']
+        return response.cookies[cookie_name]
 
     def get_header(self, response: Response, headers_name):
         assert headers_name in response.headers,  f"cant find header with name {headers_name} in the last response"
-        return response.headers['headers_name']
+        return response.headers[headers_name]
     def get_json_value(self, response: Response, name):
         try:
             response_as_dict = response.json()
--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -12,9 +12,9 @@ class TestUserAuth(BaseCase):
             'password': '1234'
         }
         response1 = requests.post('https://playground.learnqa.ru/api/user/login', data=data)
-        self.auth_sid = self.get_cookie(response1, 'auth_sid')
-        self.token = self.get_header(response1, 'x-csrf-token')
-        self.user_id_from_auth_method = self.get_json_value(response1, 'user_id')
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
     def test_user_auth_user(self):
         response2 = requests.get("https://playground.learnqa.ru/api/user/auth",
