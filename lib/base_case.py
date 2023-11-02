dfef827c2954628f1124d6fac57c8ccde624bddf
PavelMorozov <pavevlmorozov75@gmail.com>
2023-11-02 17:48:48 +0300

    updated test_user_register.py, /base_case.py - files for check

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,7 +1,27 @@
 import json.decoder
 from requests import Response
 from datetime import datetime
+import random
+import string
+
 class BaseCase:
+    def generate_name(self, length):
+        all_symbols = string.ascii_letters
+        name = ''.join(random.choice(all_symbols) for _ in range(length))
+        return name
+
+    def create_email(self, status='good'):
+        if status == 'good':
+            basepart = 'learnqa'
+            domain = 'example.com'
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            return f"{basepart}{random_part}@{domain}"
+        elif status == 'bad':
+            basepart = 'learnqa'
+            domain = 'example.com'
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            return f"{basepart}{random_part}{domain}"
+
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"cant find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
@@ -22,7 +42,7 @@ class BaseCase:
             basepart = 'learnqa'
             domain = 'example.com'
             random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-            email = f"{basepart}{random_part}@{domain}" #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            email = f"{basepart}{random_part}@{domain}"
         return {
             'password': '123',
             'username': 'learnqa',
@@ -30,5 +50,86 @@ class BaseCase:
             'lastName': 'learnqa',
             'email': email
         }
+
+    def prepare_registration_bad_data(self, bad_parametr):
+
+        if bad_parametr == 'email':
+            email_parametr = 'bad'
+            email = self.create_email(email_parametr)
+            return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+            }
+        elif bad_parametr == 'too_short_name':
+            name_lenght = 1
+            too_short_name = self.generate_name(name_lenght)
+            email = self.create_email()
+            return {
+            'password': '123',
+            'username': too_short_name,
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+            }
+        elif bad_parametr == 'too_long_name':
+            name_lenght = 255
+            too_long_name = self.generate_name(name_lenght)
+            email = self.create_email()
+            return {
+            'password': '123',
+            'username': too_long_name,
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+            }
+
+    def prepare_registration_data_without_parametr(self, parametr):
+        if parametr == 'password':
+            email = self.create_email()
+            return {
+                'username': 'learnqa',
+                'firstName': 'learnqa',
+                'lastName': 'learnqa',
+                'email': email
+            }
+        elif parametr == 'username':
+            email = self.create_email()
+            return {
+                'password': '123',
+                'firstName': 'learnqa',
+                'lastName': 'learnqa',
+                'email': email
+            }
+        elif parametr == 'firstName':
+            email = self.create_email()
+            return {
+                'username': 'learnqa',
+                'password': '123',
+                'lastName': 'learnqa',
+                'email': email
+            }
+        elif parametr == 'lastName':
+            email = self.create_email()
+            return {
+                'username': 'learnqa',
+                'password': '123',
+                'firstName': 'learnqa',
+                'email': email
+            }
+        elif parametr == 'email':
+            return {
+                'username': 'learnqa',
+                'password': '123',
+                'firstName': 'learnqa',
+                'lastName': 'learnqa'
+            }
+
+
+
+
+
 
 
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,3 +1,4 @@
+import pytest
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertion
@@ -5,20 +6,9 @@ from lib.assertions import Assertion
 
 class TestUserRegister(BaseCase):
 
-    # def setup(self):
-    #     base_part = 'learnqa'
-    #     domain = 'example.com'
-    #     random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-    #     self.email = f"{base_part}{random_part}@{domain}" #!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    missing_params = ['password', 'username', 'firstName', 'lastName', 'email']
 
     def test_create_user_successfully(self):
-        # data = {
-        #     'password': '123',
-        #     'username': 'learnqa',
-        #     'firstName': 'learnqa',
-        #     'lastName': 'learnqa',
-        #     'email': self.email
-        # }
 
         data = self.prepare_registration_data()
         response = MyRequests.post("/user/", data=data)
@@ -33,3 +23,32 @@ class TestUserRegister(BaseCase):
         # ответ приходит с буквой d в начале и требует раскодировки
         assert response.content.decode('utf-8') == f"Users with email 'winkotov@example.com' already exists",\
             f"Unexpected response {response.content}"
+
+    def test_create_user_with_too_short_name(self):
+        bad_parametr = 'too_short_name'
+        data = self.prepare_registration_bad_data(bad_parametr)
+        response = MyRequests.post("/user/", data=data)
+        Assertion.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == f"The value of 'username' field is too short", \
+        f"Unexpected response {response.content}"
+
+    def test_create_user_with_too_long_name(self):
+        bad_parametr = 'too_long_name'
+        data = self.prepare_registration_bad_data(bad_parametr)
+        response = MyRequests.post("/user/", data=data)
+        Assertion.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == f"The value of 'username' field is too long", \
+        f"Unexpected response {response.content}"
+
+    @pytest.mark.parametrize('parametr', missing_params)
+    def test_create_user_without_parametr(self, parametr):
+        data = self.prepare_registration_data_without_parametr(parametr)
+        response = MyRequests.post("/user/", data=data)
+        Assertion.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == f"The following required params are missed: {parametr}", \
+            f"Unexpected response {response.content}"
+
+
+
+
+
